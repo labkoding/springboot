30cb0e78ab52aab26b24b163b5fdd00856eb3b47
nofrets <opetstudio@gmail.com>
2023-04-27 09:48:15 +0800

    develop

--- src/main/java/id/ac/prisma/topikkhusus/backend/model/SubmitPaymentResponse.java
+++ src/main/java/id/ac/prisma/topikkhusus/backend/model/SubmitPaymentResponse.java
@@ -1,22 +1,12 @@
 package id.ac.prisma.topikkhusus.backend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SubmitPaymentResponse {
     private String status;
     private String transactionId;
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
+    private TbTransactions createdData;
 }
---
+++ src/main/java/id/ac/prisma/topikkhusus/backend/model/TbTransactionsRepository.java
@@ -0,0 +1,6 @@
+package id.ac.prisma.topikkhusus.backend.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TbTransactionsRepository extends JpaRepository<TbTransactions, Integer> {
+}
